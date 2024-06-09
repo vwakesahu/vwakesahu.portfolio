08d179def0184267500789cbe448ff2701fc5672
Vivek Sahu <viveksahu1762@gmail.com>
2024-06-09 22:07:26 +0530

    Data for each project

---
+++ src/utils/data.js
@@ -0,0 +1,65 @@
+import { FaReact } from "react-icons/fa";
+import { IoLogoFirebase } from "react-icons/io5";
+import { MdOutlineMail } from "react-icons/md";
+import { RiNextjsFill } from "react-icons/ri";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { DiMongodb } from "react-icons/di";
+import { SiExpress } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa6";
+
+export const jobshunt = {
+  title: "Jobshunt",
+  description: `So, here&rsquo;s the story of Jobshunt. I got this 
+    <span class="text-secondary-foreground font-semibold">freelancing gig</span> 
+    to create <span class="text-secondary-foreground font-semibold">a job portal for web3 folks.</span> 
+    Everything was going great until, out of nowhere, the client disappeared. Yep, just vanished! 
+    I was left holding the bag (or, in this case, the code). But instead of giving up, I decided to 
+    turn this into a <span class="text-secondary-foreground font-semibold">personal project.</span> In my third 
+    year of college, I rolled up my sleeves and built the whole thing by myself. 
+    <span class="text-secondary-foreground font-semibold">I used ReactJS for the frontend, Firebase for the backend, and 
+    EmailJS to handle email communications.</span> Jobshunt is designed to help people in the web3 community find jobs. 
+    Whether you&rsquo;re into blockchain, crypto, or any other web3 tech, this portal is for you. Building it was a 
+    fantastic learning experience, and <span class="text-secondary-foreground font-semibold">it taught me a lot about 
+    persistence and problem-solving.</span> So, if you&rsquo;re checking out my portfolio, know that Jobshunt is a project born out of 
+    a little chaos and a lot of determination. You can find the <a href='https://github.com/vwakesahu/jobshunt' target='_blank' class="font-semibold text-blue-500 cursor-pointer">source code</a> over here.`,
+  icons: [FaReact, IoLogoFirebase, RiTailwindCssFill, MdOutlineMail],
+  livePreview: "https://jobshunt-six.vercel.app/",
+  navItems: ["Jobshunt", "Home"],
+  imgPaths: ["/jobshunt/desk-main.svg", "/jobshunt/mob-main.svg"],
+};
+
+export const fbe = {
+  title: "Frames",
+  description: `Hey! Let's kick things off with a quick joke: 
+    Why did the photo go to therapy? Because it had too many <span class="text-secondary-foreground font-semibold">frames of mind!</span> 😄 
+    Now, onto <span class="text-secondary-foreground font-semibold">Frames</span>
+
+    So, what's the scoop with <span class="text-secondary-foreground font-semibold">Frames</span>?
+    Besides being your go-to for framing your favorite pics, we've got some neat features.
+    Ever wanted to grab a seat at an exhibition hassle-free? <span class="text-secondary-foreground font-semibold">Frames</span>'s got your back.
+    Book your spot, pay up with <span class="text-secondary-foreground font-semibold">Razorpay</span>, and bam – <span class="text-secondary-foreground font-semibold">your ticket's there on your dashboard, ready for action.</span>
+    And here's the cherry on top: <span class="text-secondary-foreground font-semibold">we've got collage presets</span> for your pics and even a video-call feature to make sure your photos look ace.
+    Because let's be real, memories deserve the VIP treatment, right?<br>You can find the <a href='https://frames-by-engineer.vercel.app/' target='_blank' class="font-semibold text-blue-500 cursor-pointer">live website</a> over here.`,
+  icons: [RiNextjsFill, IoLogoFirebase, RiTailwindCssFill, MdOutlineMail],
+  livePreview: "https://frames-by-engineer.vercel.app/",
+  navItems: ["Frames", "Home"],
+  imgPaths: ["/fbe/desk-main.svg", "/fbe/mob-main.svg"],
+};
+
+export const streamer = {
+  title: "Streamer",
+  description: `So, what's the deal with Streamer? Well, we've built it using some fancy tech called <span class="text-secondary-foreground font-semibold">MERN (MongoDB, Express.js, React.js, Node.js) </span>and added this thing called <span class="text-secondary-foreground font-semibold">HLS video streaming</span> to make your viewing experience super smooth. It's like magic for your eyes!
+
+But enough tech talk, let's dive into the fun stuff! Whether you're rolling on the floor <span class="text-secondary-foreground font-semibold">laughing at epic fails or sharing your own hilarious gaming moments, Streamer's got you covered. </span>It's your go-to spot for all things gaming and laughs.
+
+So,<span class="text-secondary-foreground font-semibold"> grab your controller, hit record, </span>and let's make some gaming memories together. Streamer – where every laugh is a win!
+<br>You can find the <a href='https://github.com/vvs-pvt-ltd/streamer-ui.git' target='_blank' class="font-semibold text-blue-500 cursor-pointer">github repository</a> over here.`,
+  icons: [DiMongodb, SiExpress, FaReact, FaNodeJs],
+  livePreview: "https://streamer-pi.vercel.app/",
+  navItems: ["Streamer", "Home"],
+  imgPaths: [
+    "/streamer/desk-main.svg",
+    "/streamer/mob-main.svg",
+    "/streamer/mob-2.svg",
+  ],
+};
